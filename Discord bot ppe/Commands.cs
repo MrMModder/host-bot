2603a9504862481ba560d962d6ad2b63c54ac7fc
MrMModder <35263058+MrMModder@users.noreply.github.com>
2018-01-09 14:45:37 +0100

    Add files via upload

---
+++ Discord bot ppe/Commands.cs
@@ -0,0 +1,91 @@
+﻿using Discord.Commands;
+using Discord;
+using Discord.Net;
+using Discord.Webhook;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPExDBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("Help")]
+        public async Task HelpAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("!FakkePPE @Name", "Says if someone is a fake PPE", false)
+                .AddField("!GiveRole @Name Role", "Assings someone a role, only if you have permission", false)
+                .AddField("!Tableflip", "Just try it", false)
+                .AddField("!Kick @Name", "Kicks Someone, Only with permission", false)
+                .AddField("!Meme", "What do you think this does?")
+                .AddField("? Prefix", "If you type ? before a message it gets an upvote and a downvote");
+            await ReplyAsync("", false, builder.Build());
+        }
+        [Command("supersecretppecommandxdlmao")]
+        public async Task XDASYNC()
+        {
+            var user = Context.User.Username;
+            var guild = Context.Guild;
+            var channel = guild.DefaultChannel;
+            string xd = ("HOLY BALLS " + user + " TRIGGERED THE SPECIAL COMMAND");
+            await channel.SendMessageAsync(xd);
+        }
+
+        [Command("FakePPE")]
+        public async Task PingAsync([Remainder] string name = "Dylan")
+        {
+
+
+            //Context.User;
+            //Context.Client;
+            //Context.Guild;
+            //Context.Message;
+            //Context.Channel
+            await ReplyAsync($"{name} is a fake ppe");
+        }
+        [Command("GiveRole")]
+        public async Task RoleAsync(IGuildUser user, [Remainder] string roleName)
+        {
+            var CommandCaller = Context.User as IGuildUser;
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == roleName);
+            if (CommandCaller.GuildPermissions.ManageRoles == true)
+            {
+
+                await (user as IGuildUser).AddRoleAsync(role);
+            }
+        }
+        [Command("TableFlip")]
+        public async Task FlipAsync()
+        {
+            await ReplyAsync(" ┬─┬ ノ( ゜-゜ノ)");
+            await Task.Delay(2000);
+            await ReplyAsync(" (╯°□°）╯︵ ┻━┻");
+
+        }
+        [Command("Kick"), RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task KickAsync(IGuildUser user)
+        {
+            await (user as IGuildUser).KickAsync();
+        }
+        [Command("Meme")]
+        public async Task MemeSync()
+        {
+            await Context.Channel.SendFileAsync("meme/xd1.jpg");
+            await Context.User.SendMessageAsync("You Filthy Memer");
+        }
+        [Command("Max")]
+        public async Task MaxSync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.AddField("Oryxshop.net", "DeCaS aRe iN sToCk nOw!");
+            await ReplyAsync("",false, builder.Build());
+            
+        }
+
+
+    }
+}
---
+++ Discord bot ppe/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.Webhook;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Net.Providers.WS4Net;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Rest;
+using System.Linq;
+
+
+namespace PPExD_Bot
+{
+
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+        public async Task RunBotAsync()
+        {
+           
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                WebSocketProvider = WS4NetProvider.Instance,
+            });
+
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            String botToken = "Mzk5NTcwODE5NzQwODYwNDE2.DTPWzA.pgzpwBJ-pC0y-h2M2FddbcYI3-M";
+
+            //event subscriptions
+            _client.Log += Log;
+            _client.UserJoined += AnnounceUserJoined;
+            _client.MessageReceived += AutoVoter;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await _client.SetGameAsync("DREAM PPE XD");
+
+
+
+            await Task.Delay(-1);
+
+        }
+
+        private async Task AutoVoter(SocketMessage messagerecieved)
+        {
+            int argPos = 0;
+            var xd = messagerecieved as SocketUserMessage;
+
+
+
+
+           if (messagerecieved.Channel.Name == "suggestions")
+            {
+                var ThumbsUp = new Emoji("👍");
+                var ThumbsDown = new Emoji("👎");
+                var rMessage = (RestUserMessage)await messagerecieved.Channel.GetMessageAsync(messagerecieved.Id);
+                await rMessage.AddReactionAsync(ThumbsUp);
+                await rMessage.AddReactionAsync(ThumbsDown, new RequestOptions());
+
+
+            }
+            else if (xd.HasStringPrefix("?", ref argPos))
+            {
+                var ThumbsUp = new Emoji("👍");
+                var ThumbsDown = new Emoji("👎");
+                var rMessage = (RestUserMessage)await messagerecieved.Channel.GetMessageAsync(messagerecieved.Id);
+                await rMessage.AddReactionAsync(ThumbsUp);
+                await rMessage.AddReactionAsync(ThumbsDown, new RequestOptions());
+            }
+        }
+
+        private async Task AnnounceUserJoined(SocketGuildUser user)
+        {
+            string roleName = "unverified";
+            var role = user.Guild.Roles.FirstOrDefault(x => x.Name == roleName);
+            await user.AddRoleAsync(role);
+            await user.SendMessageAsync("Welcome to the PPE server, Don't forget to get verfified to see all of the channels!");
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
